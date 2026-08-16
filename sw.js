59f12a7010c512d41a7083fc5002596a8653c7db
Julien Froment <ju.c.froment@gmail.com>
2026-08-16 20:08:53 +0200

    fix: switch service worker to a network-first strategy

--- sw.js
+++ sw.js
@@ -28,6 +28,12 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
